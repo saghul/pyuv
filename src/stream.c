57be8201b808ed56bda50f888eb38dd62c3f520c
saghul <saghul@gmail.com>
2011-11-10 00:26:13 +0100

    Fixed calling tp_clear on IOStream child classes

--- src/stream.c
+++ src/stream.c
@@ -365,7 +365,7 @@ IOStream_tp_dealloc(IOStream *self)
     if (!self->closed) {
         uv_close((uv_handle_t *)self->uv_stream, on_iostream_close);
     }
-    IOStream_tp_clear(self);
+    Py_TYPE(self)->tp_clear((PyObject *)self);
     Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
