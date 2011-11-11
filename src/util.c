f5047d42ac4a1c1f7afb13d1b5ae99d8f8f85863
saghul <saghul@gmail.com>
2011-11-11 23:01:30 +0100

    Added get_free_memory / get_total_memory functions

--- src/util.c
+++ src/util.c
@@ -12,9 +12,37 @@ Util_func_hrtime(PyObject *self)
 }
 
 
+static PyObject *
+Util_func_get_free_memory(PyObject *self)
+{
+    PyObject *val;
+#if SIZEOF_TIME_T > SIZEOF_LONG
+    val = PyLong_FromLongLong((PY_LONG_LONG)uv_get_free_memory());
+#else
+    val = PyInt_FromLong((long)uv_get_free_memory());
+#endif
+    return val;
+}
+
+
+static PyObject *
+Util_func_get_total_memory(PyObject *self)
+{
+    PyObject *val;
+#if SIZEOF_TIME_T > SIZEOF_LONG
+    val = PyLong_FromLongLong((PY_LONG_LONG)uv_get_total_memory());
+#else
+    val = PyInt_FromLong((long)uv_get_total_memory());
+#endif
+    return val;
+}
+
+
 static PyMethodDef
 Util_methods[] = {
     { "hrtime", (PyCFunction)Util_func_hrtime, METH_NOARGS, "High resolution time." },
+    { "get_free_memory", (PyCFunction)Util_func_get_free_memory, METH_NOARGS, "Get system free memory." },
+    { "get_total_memory", (PyCFunction)Util_func_get_total_memory, METH_NOARGS, "Get system total memory." },
     { NULL }
 };
 
