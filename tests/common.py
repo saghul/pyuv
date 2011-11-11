cbbd0e4f1106d31ce76e4e4fc75fae5f8d676383
saghul <saghul@gmail.com>
2011-11-11 09:53:11 +0100

    Use TestSuite and TestLoader from unittest to run the test suite

--- tests/common.py
+++ tests/common.py
@@ -1,4 +1,5 @@
 
+import os
 import sys
 sys.path.insert(0, '../')
 import unittest
@@ -8,6 +9,28 @@ TEST_PIPE = 'test-pipe'
 BAD_PIPE = '/pipe/that/does/not/exist'
 
 
+loader = unittest.TestLoader()
+suites = []
+
+class TestCaseMeta(type):
+    def __init__(cls, name, bases, dic):
+        super(TestCaseMeta, cls).__init__(name, bases, dic)
+        suites.append(loader.loadTestsFromTestCase(cls))
+
 class UVTestCase(unittest.TestCase):
-    pass
+    __metaclass__ = TestCaseMeta
+
+
+def load_tests():
+    loaded_modules = []
+    mod_list = [f for f in os.listdir(os.path.dirname(__file__)) if f.startswith('test_')]
+    for mod in mod_list:
+        try:
+            __import__(mod)
+        except ImportError:
+            pass
+        else:
+            loaded_modules.append(mod)
+    return loaded_modules
+
 
