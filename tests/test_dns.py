fb01ec2a7922b214fcc9ba9edde4b7cc431a1b97
saghul <saghul@gmail.com>
2011-11-14 01:09:19 +0100

    Added tests for DNS module

---
+++ tests/test_dns.py
@@ -0,0 +1,50 @@
+
+import common
+import socket
+import unittest
+
+import pyuv
+
+
+class DNSTest(common.UVTestCase):
+
+    def getaddrinfo_cb(self, resolver, status, result):
+        self.assertEqual(status, 0)
+
+    def test_getaddrinfo(self):
+        loop = pyuv.Loop.default_loop()
+        resolver = pyuv.dns.DNSResolver(loop)
+        resolver.getaddrinfo(self.getaddrinfo_cb, 'localhost', 0, socket.AF_INET)
+        loop.run()
+
+    def gethostbyaddr_cb(self, resolver, status, timeouts, name, aliases, result):
+        self.assertEqual(status, 0)
+
+    def test_gethostbyaddr(self):
+        loop = pyuv.Loop.default_loop()
+        resolver = pyuv.dns.DNSResolver(loop)
+        resolver.gethostbyaddr(self.gethostbyaddr_cb, '127.0.0.1')
+        loop.run()
+
+    def gethostbyname_cb(self, resolver, status, timeouts, name, aliases, result):
+        self.assertEqual(status, 0)
+
+    def test_gethostbyname(self):
+        loop = pyuv.Loop.default_loop()
+        resolver = pyuv.dns.DNSResolver(loop)
+        resolver.gethostbyname(self.gethostbyname_cb, 'localhost', socket.AF_INET)
+        loop.run()
+
+    def getnameinfo_cb(self, resolver, status, timeouts, node, service):
+        self.assertEqual(status, 0)
+
+    def test_getnameinfo(self):
+        loop = pyuv.Loop.default_loop()
+        resolver = pyuv.dns.DNSResolver(loop)
+        resolver.getnameinfo(self.getnameinfo_cb, '127.0.0.1', 0, pyuv.dns.ARES_NI_LOOKUPHOST)
+        loop.run()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
