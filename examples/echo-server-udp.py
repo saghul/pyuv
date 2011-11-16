b5191e09392d77bfd36b6bc41af6f40807b8f9a9
saghul <saghul@gmail.com>
2011-11-16 23:51:40 +0100

    Added examples

---
+++ examples/echo-server-udp.py
@@ -0,0 +1,43 @@
+
+import os
+import signal
+import threading
+import pyuv
+
+
+def on_read(handle, (ip, port), data):
+    data = data.strip()
+    if data:
+	handle.send(data+os.linesep, (ip, port))
+
+def async_exit(async, data):
+    async.close()
+    signal_h.close()
+    server.close()
+
+def signal_cb(sig, frame):
+    async.send(async_exit)
+
+
+print "PyUV version %s" % pyuv.__version__
+
+loop = pyuv.Loop()
+async = pyuv.Async(loop)
+
+server = pyuv.UDP(loop)
+server.bind(("0.0.0.0", 1234))
+server.start_recv(on_read)
+
+signal_h = pyuv.Signal(loop)
+signal_h.start()
+
+t = threading.Thread(target=loop.run)
+t.start()
+
+signal.signal(signal.SIGINT, signal_cb)
+signal.pause()
+
+t.join()
+
+print "Stopped!"
+
---
+++ examples/echo-server.py
@@ -0,0 +1,62 @@
+
+import os
+import signal
+import threading
+import pyuv
+
+
+def on_client_shutdown(client):
+    client.close()
+    clients.remove(client)
+
+def on_read(client, data):
+    if data is None:
+        client.close()
+        clients.remove(client)
+        return
+    data = data.strip()
+    if not data:
+        return
+    client.write(data+os.linesep)
+    client.shutdown(on_client_shutdown)
+
+def on_connection(server):
+    client = server.accept()
+    clients.append(client)
+    client.start_read(on_read)
+
+def async_exit(async, data):
+    [c.close() for c in clients]
+    async.close()
+    signal_h.close()
+    server.close()
+
+def signal_cb(sig, frame):
+    async.send(async_exit)
+
+
+print "PyUV version %s" % pyuv.__version__
+
+loop = pyuv.Loop()
+
+async = pyuv.Async(loop)
+clients = []
+
+server = pyuv.TCP(loop)
+server.bind(("0.0.0.0", 1234))
+server.listen(on_connection)
+
+signal_h = pyuv.Signal(loop)
+signal_h.start()
+
+t = threading.Thread(target=loop.run)
+t.start()
+
+signal.signal(signal.SIGINT, signal_cb)
+signal.pause()
+
+t.join()
+
+print "Stopped!"
+
+
