d83d6c4e146679c23dd947d56dc6609448a1696d
saghul <saghul@gmail.com>
2011-11-26 16:04:58 +0100

    Added stdin/stdout echo examples using Pipe and TTY

---
+++ examples/echo-pipe.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+import pyuv
+
+
+def on_pipe_read(handle, data):
+    data = data.strip()
+    if data == "exit":
+        pipe_stdin.close()
+        pipe_stdout.close()
+    else:
+        pipe_stdout.write([data, os.linesep])
+
+
+loop = pyuv.Loop.default_loop()
+
+pipe_stdin = pyuv.Pipe(loop)
+pipe_stdin.open(sys.stdin.fileno())
+pipe_stdin.start_read(on_pipe_read)
+
+pipe_stdout = pyuv.Pipe(loop)
+pipe_stdout.open(sys.stdout.fileno())
+
+loop.run()
+
---
+++ examples/echo-tty.py
@@ -0,0 +1,27 @@
+
+import os
+import sys
+import pyuv
+
+
+def on_tty_read(handle, data):
+    data = data.strip()
+    if data == "exit":
+        tty_stdin.close()
+        tty_stdout.close()
+    else:
+        tty_stdout.write([data, os.linesep])
+
+loop = pyuv.Loop.default_loop()
+
+tty_stdin = pyuv.TTY(loop, sys.stdin.fileno())
+tty_stdin.start_read(on_tty_read)
+
+tty_stdout = pyuv.TTY(loop, sys.stdout.fileno())
+
+print "Window size: (%d, %d)" % tty_stdin.get_winsize()
+
+loop.run()
+
+pyuv.TTY.reset_mode()
+
