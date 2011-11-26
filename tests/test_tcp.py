97b8a19dc0b7225b48616621c4c5f872404437f7
saghul <saghul@gmail.com>
2011-11-26 02:37:47 +0100

    Added test for sending a list of strings over a TCP connection

--- tests/test_tcp.py
+++ tests/test_tcp.py
@@ -52,7 +52,47 @@ class TCPTest(common.UVTestCase):
         self.assertEquals(data, "PING")
         client.close()
 
-    def test_pipe1(self):
+    def test_tcp1(self):
+        self.server = pyuv.TCP(self.loop)
+        self.server.bind(("0.0.0.0", TEST_PORT))
+        self.server.listen(self.on_connection)
+        self.client = pyuv.TCP(self.loop)
+        self.client.connect(("127.0.0.1", TEST_PORT), self.on_client_connection)
+        self.loop.run()
+
+
+class TCPTestList(common.UVTestCase):
+
+    def setUp(self):
+        self.loop = pyuv.Loop.default_loop()
+        self.server = None
+        self.client = None
+        self.client_connections = []
+
+    def on_connection(self, server):
+        client = server.accept()
+        self.client_connections.append(client)
+        client.start_read(self.on_client_connection_read)
+        client.write(["PING"+os.linesep])
+
+    def on_client_connection_read(self, client, data):
+        if data is None:
+            client.close()
+            self.client_connections.remove(client)
+            self.server.close()
+            return
+
+    def on_client_connection(self, client, status):
+        self.assertEquals(status, 0)
+        client.start_read(self.on_client_read)
+
+    def on_client_read(self, client, data):
+        self.assertNotEqual(data, None)
+        data = data.strip()
+        self.assertEquals(data, "PING")
+        client.close()
+
+    def test_tcp1(self):
         self.server = pyuv.TCP(self.loop)
         self.server.bind(("0.0.0.0", TEST_PORT))
         self.server.listen(self.on_connection)
@@ -99,7 +139,7 @@ class TCPShutdownTest(common.UVTestCase):
         self.assertEquals(data, "PING")
         client.shutdown(self.on_client_shutdown)
 
-    def test_pipe1(self):
+    def test_tcp1(self):
         self.shutdown_cb_called = 0
         self.close_cb_called = 0
         self.server = pyuv.TCP(self.loop)
