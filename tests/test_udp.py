17ff6d29938e51a08ae530ee712e31ede225b4d3
saghul <saghul@gmail.com>
2011-12-15 05:17:21 +0100

    Added UDP multicast test

--- tests/test_udp.py
+++ tests/test_udp.py
@@ -8,6 +8,7 @@ import pyuv
 
 TEST_PORT = 12345
 TEST_PORT2 = 12346
+MULTICAST_ADDRESS = "239.255.0.1"
 
 class UDPTest(common.UVTestCase):
 
@@ -85,6 +86,38 @@ class UDPTestList(common.UVTestCase):
         timer.start(self.timer_cb, 1, 0)
         self.loop.run()
         self.assertEqual(self.on_close_called, 3)
+
+
+class UDPTestMulticast(common.UVTestCase):
+
+    def setUp(self):
+        self.loop = pyuv.Loop.default_loop()
+        self.server = None
+        self.client = None
+        self.received_data = None
+
+    def on_close(self, handle):
+        self.on_close_called += 1
+
+    def on_client_recv(self, handle, (ip, port), data):
+        self.received_data = data.strip()
+        self.client.set_membership(MULTICAST_ADDRESS, pyuv.UV_LEAVE_GROUP)
+        self.client.close(self.on_close)
+
+    def on_server_send(self, handle, status):
+        handle.close(self.on_close)
+
+    def test_udp_multicast(self):
+        self.on_close_called = 0
+        self.server = pyuv.UDP(self.loop)
+        self.client = pyuv.UDP(self.loop)
+        self.client.bind((MULTICAST_ADDRESS, TEST_PORT))
+        self.client.set_membership(MULTICAST_ADDRESS, pyuv.UV_JOIN_GROUP)
+        self.client.start_recv(self.on_client_recv)
+        self.server.send("PING", (MULTICAST_ADDRESS, TEST_PORT), self.on_server_send)
+        self.loop.run()
+        self.assertEqual(self.on_close_called, 2)
+        self.assertEquals(self.received_data, "PING")
 
 
 if __name__ == '__main__':
