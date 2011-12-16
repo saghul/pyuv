d07585ebb1c460311e8294da924a1aee4a9559ee
saghul <saghul@gmail.com>
2011-12-16 10:54:33 +0100

    Added unlink function to fs module

--- src/fs.c
+++ src/fs.c
@@ -128,6 +128,39 @@ end:
 }
 
 
+static void
+unlink_cb(uv_fs_t* req) {
+    PyGILState_STATE gstate = PyGILState_Ensure();
+    ASSERT(req);
+
+    fs_req_data_t *req_data = (fs_req_data_t*)(req->data);
+
+    PyObject *result, *errorno;
+
+    result = PyInt_FromLong((long)req->result);
+    if (req->result < 0) {
+        errorno = PyInt_FromLong((long)req->errorno);
+    } else {
+        errorno = PyInt_FromLong(0);
+    }
+
+    PyObject *cb_result = PyObject_CallFunctionObjArgs(req_data->callback, req_data->loop, req_data->data, result, errorno, NULL);
+    if (cb_result == NULL) {
+        PyErr_WriteUnraisable(req_data->callback);
+    }
+    Py_XDECREF(cb_result);
+
+    uv_fs_req_cleanup(req);
+    Py_DECREF(req_data->loop);
+    Py_DECREF(req_data->callback);
+    Py_DECREF(req_data->data);
+    PyMem_Free(req_data);
+    PyMem_Free(req);
+
+    PyGILState_Release(gstate);
+}
+
+
 static PyObject *
 stat_func(PyObject *self, PyObject *args, PyObject *kwargs, int type)
 {
@@ -211,10 +244,76 @@ FS_func_lstat(PyObject *self, PyObject *args, PyObject *kwargs)
 }
 
 
+static PyObject *
+FS_func_unlink(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    int r;
+    char *path;
+    Loop *loop;
+    PyObject *callback;
+    PyObject *data = Py_None;
+    uv_fs_t *fs_req = NULL;
+    fs_req_data_t *req_data = NULL;
+
+    static char *kwlist[] = {"loop", "path", "callback", "data", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O!sO|O:stat", kwlist, &LoopType, &loop, &path, &callback, &data)) {
+        return NULL;
+    }
+
+    if (!PyCallable_Check(callback)) {
+        PyErr_SetString(PyExc_TypeError, "a callable or None is required");
+        return NULL;
+    }
+
+    fs_req = PyMem_Malloc(sizeof(uv_fs_t));
+    if (!fs_req) {
+        PyErr_NoMemory();
+        goto error;
+    }
+
+    req_data = PyMem_Malloc(sizeof(fs_req_data_t));
+    if (!req_data) {
+        PyErr_NoMemory();
+        goto error;
+    }
+
+    Py_INCREF(loop);
+    Py_INCREF(callback);
+    Py_INCREF(data);
+
+    req_data->loop = loop;
+    req_data->callback = callback;
+    req_data->data = data;
+
+    fs_req->data = (void *)req_data;
+    r = uv_fs_unlink(loop->uv_loop, fs_req, path, unlink_cb);
+    if (r != 0) {
+        raise_uv_exception(loop, PyExc_FSError);
+        goto error;
+    }
+
+    Py_RETURN_NONE;
+
+error:
+    if (fs_req) {
+        PyMem_Free(fs_req);
+    }
+    if (req_data) {
+        Py_DECREF(loop);
+        Py_DECREF(callback);
+        Py_DECREF(data);
+        PyMem_Free(req_data);
+    }
+    return NULL;
+}
+
+
 static PyMethodDef
 FS_methods[] = {
     { "stat", (PyCFunction)FS_func_stat, METH_VARARGS|METH_KEYWORDS, "stat" },
     { "lstat", (PyCFunction)FS_func_lstat, METH_VARARGS|METH_KEYWORDS, "lstat" },
+    { "unlink", (PyCFunction)FS_func_unlink, METH_VARARGS|METH_KEYWORDS, "Remove a file from the filesystem." },
     { NULL }
 };
 
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -23,28 +23,78 @@ class FSTest(common.UVTestCase):
         os.remove(TEST_LINK)
 
     def stat_error_cb(self, loop, data, result, errorno, stat_result):
-        self.assertEqual(result, -1)
-        self.assertNotEqual(errorno, 0)
+        self.result = result
+        self.errorno = errorno
 
     def test_stat_error(self):
+        self.result = None
+        self.errorno = None
         pyuv.fs.stat(self.loop, BAD_FILE, self.stat_error_cb)
         self.loop.run()
+        self.assertEqual(self.result, -1)
+        self.assertEqual(self.errorno, pyuv.errno.UV_ENOENT)
 
     def stat_cb(self, loop, data, result, errorno, stat_result):
-        self.assertEqual(result, 0)
-        self.assertEqual(errorno, 0)
+        self.result = result
+        self.errorno = errorno
 
     def test_stat(self):
+        self.result = None
+        self.errorno = None
         pyuv.fs.stat(self.loop, TEST_FILE, self.stat_cb)
         self.loop.run()
+        self.assertEqual(self.result, 0)
+        self.assertEqual(self.errorno, 0)
 
     def lstat_cb(self, loop, data, result, errorno, stat_result):
-        self.assertEqual(result, 0)
-        self.assertEqual(errorno, 0)
+        self.result = result
+        self.errorno = errorno
 
     def test_lstat(self):
+        self.result = None
+        self.errorno = None
         pyuv.fs.lstat(self.loop, TEST_LINK, self.lstat_cb)
         self.loop.run()
+        self.assertEqual(self.result, 0)
+        self.assertEqual(self.errorno, 0)
+
+
+class FSTestUnlink(common.UVTestCase):
+
+    def setUp(self):
+        self.loop = pyuv.Loop.default_loop()
+        with open(TEST_FILE, 'w') as f:
+            f.write('test')
+
+    def tearDown(self):
+        try:
+            os.remove(TEST_FILE)
+        except OSError:
+            pass
+
+    def bad_unlink_cb(self, loop, data, result, errorno):
+        self.result = result
+        self.errorno = errorno
+
+    def test_bad_unlink(self):
+        self.result = None
+        self.errorno = None
+        pyuv.fs.unlink(self.loop, BAD_FILE, self.bad_unlink_cb)
+        self.loop.run()
+        self.assertEqual(self.result, -1)
+        self.assertEqual(self.errorno, pyuv.errno.UV_ENOENT)
+
+    def unlink_cb(self, loop, data, result, errorno):
+        self.result = result
+        self.errorno = errorno
+
+    def test_unlink(self):
+        self.result = None
+        self.errorno = None
+        pyuv.fs.unlink(self.loop, TEST_FILE, self.unlink_cb)
+        self.loop.run()
+        self.assertEqual(self.result, 0)
+        self.assertEqual(self.errorno, 0)
 
 
 if __name__ == '__main__':
