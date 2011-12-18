ceb8d496e8ccb6e3e5dafa9646201acec357e0af
saghul <saghul@gmail.com>
2011-12-19 00:22:07 +0100

    Refactored build system. Added flags for re-downloading libuv and for forcing a rebuild

--- setup.py
+++ setup.py
@@ -1,94 +1,10 @@
 # coding=utf8
 
-import os
-import shutil
-import subprocess
-import sys
-
-from distutils import log
-from distutils.command.build_ext import build_ext
 from distutils.core import setup, Extension
-from distutils.errors import DistutilsError
+from setup_libuv import libuv_build_ext, libuv_sdist
 
 
 __version__ = "0.1.0"
-
-class pyuv_build_ext(build_ext):
-    libuv_repo = 'https://github.com/joyent/libuv.git'
-    libuv_branch = 'master'
-    libuv_revision = '8e50b60'
-    libuv_patches = []
-
-    @staticmethod
-    def exec_process(cmdline, silent=True, input=None, **kwargs):
-        """Execute a subprocess and returns the returncode, stdout buffer and stderr buffer.
-        Optionally prints stdout and stderr while running."""
-        try:
-            sub = subprocess.Popen(cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
-            stdout, stderr = sub.communicate(input=input)
-            returncode = sub.returncode
-            if not silent:
-                sys.stdout.write(stdout)
-                sys.stderr.write(stderr)
-        except OSError,e:
-            if e.errno == 2:
-                raise DistutilsError('"%s" is not present on this system' % cmdline[0])
-            else:
-                raise
-        if returncode != 0:
-            raise DistutilsError('Got return value %d while executing "%s", stderr output was:\n%s' % (returncode, " ".join(cmdline), stderr.rstrip("\n")))
-        return stdout
-
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-        self.get_libuv()
-        self.include_dirs.append(os.path.join(self.libuv_dir, 'include'))
-        self.library_dirs.append(self.libuv_dir)
-        if sys.platform.startswith('linux'):
-            self.libraries.append('rt')
-        elif sys.platform == 'darwin':
-            self.extensions[0].extra_link_args = ['-framework', 'CoreServices']
-        self.extensions[0].extra_objects = [os.path.join(self.libuv_dir, 'uv.a')]
-
-    def get_libuv(self):
-        self.libuv_dir = os.path.join(self.build_temp, 'libuv')
-        #self.debug_mode =  bool(self.debug) or hasattr(sys, 'gettotalrefcount')
-        def download_libuv():
-            log.info('Downloading libuv...')
-            self.exec_process(['git', 'clone', '-b', self.libuv_branch, self.libuv_repo, self.libuv_dir])
-            self.exec_process(['git', 'checkout', self.libuv_revision], cwd=self.libuv_dir)
-        def patch_libuv():
-            log.info('Patching libuv...')
-            for patch_file in self.libuv_patches:
-                self.exec_process(['patch', '--forward', '-d', self.libuv_dir, '-p0', '-i', os.path.abspath(patch_file)])
-        def build_libuv():
-            cflags = '-fPIC'
-            env = os.environ.copy()
-            env['CFLAGS'] = ' '.join(x for x in (cflags, env.get('CFLAGS', None)) if x)
-            log.info('Building libuv...')
-            self.exec_process(['make', 'uv.a'], cwd=self.libuv_dir, env=env)
-        def update_libuv():
-            shutil.rmtree(self.libuv_dir)
-            download_libuv()
-            patch_libuv()
-            build_libuv()
-        if not os.path.exists(self.libuv_dir):
-            download_libuv()
-            patch_libuv()
-            build_libuv()
-        else:
-            if not os.path.exists(os.path.join(self.libuv_dir, 'uv.a')):
-                log.info('libuv needs to be compiled.')
-                build_libuv()
-            else:
-                rev = self.exec_process(['git', 'rev-parse', '--short', 'HEAD'], cwd=self.libuv_dir)
-                if rev.strip() == self.libuv_revision:
-                    log.info('No need to download libuv.')
-                else:
-                    # TODO: implement a command line switch to force rebuild
-                    log.info('libuv needs to be updated.')
-                    update_libuv()
-
 
 setup(name         = "pyuv",
       version      = __version__,
@@ -104,7 +20,12 @@ setup(name         = "pyuv",
           "Operating System :: POSIX",
           "Programming Language :: C"
       ],
-      cmdclass     = {'build_ext': pyuv_build_ext},
-      ext_modules  = [Extension('pyuv', sources = ['src/pyuv.c'], define_macros=[('MODULE_VERSION', __version__), ('LIBUV_REVISION', pyuv_build_ext.libuv_revision)])]
+      cmdclass     = {'build_ext': libuv_build_ext,
+                      'sdist'    : libuv_sdist},
+      ext_modules  = [Extension('pyuv',
+                                sources = ['src/pyuv.c'],
+                                define_macros=[('MODULE_VERSION', __version__),
+                                               ('LIBUV_REVISION', libuv_build_ext.libuv_revision)]
+                     )]
      )
 
---
+++ setup_libuv.py
@@ -0,0 +1,135 @@
+
+import errno
+import os
+import shutil
+import subprocess
+import sys
+
+from distutils import log
+from distutils.command.build_ext import build_ext
+from distutils.command.sdist import sdist
+from distutils.errors import DistutilsError
+
+
+def makedirs(path):
+    try:
+        os.makedirs(path)
+    except OSError, e:
+        if e.errno==errno.EEXIST and os.path.isdir(path) and os.access(path, os.R_OK | os.W_OK | os.X_OK):
+            return
+        raise
+
+def rmtree(path):
+    try:
+        shutil.rmtree(path)
+    except OSError, e:
+        if e.errno != errno.ENOENT:
+            raise
+
+def exec_process(cmdline, silent=True, input=None, **kwargs):
+    """Execute a subprocess and returns the returncode, stdout buffer and stderr buffer.
+    Optionally prints stdout and stderr while running."""
+    try:
+        sub = subprocess.Popen(args=cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+        stdout, stderr = sub.communicate(input=input)
+        returncode = sub.returncode
+        if not silent:
+            sys.stdout.write(stdout)
+            sys.stderr.write(stderr)
+    except OSError,e:
+        if e.errno == 2:
+            raise DistutilsError('"%s" is not present on this system' % cmdline[0])
+        else:
+            raise
+    if returncode != 0:
+        raise DistutilsError('Got return value %d while executing "%s", stderr output was:\n%s' % (returncode, " ".join(cmdline), stderr.rstrip("\n")))
+    return stdout
+
+
+class libuv_build_ext(build_ext):
+    libuv_dir      = os.path.join('deps', 'libuv')
+    libuv_repo     = 'https://github.com/joyent/libuv.git'
+    libuv_branch   = 'master'
+    libuv_revision = '8e50b60'
+    libuv_patches  = []
+
+    user_options = build_ext.user_options
+    user_options.extend([
+        ("libuv-clean-compile", None, "Clean libuv tree before compilation"),
+        ("libuv-force-fetch", None, "Remove libuv (if present) and fetch it again")
+    ])
+    boolean_options = build_ext.boolean_options
+    boolean_options.extend(["libuv-clean-compile", "libuv-force-fetch"])
+
+    def initialize_options(self):
+        build_ext.initialize_options(self)
+        self.libuv_clean_compile = 0
+        self.libuv_force_fetch = 0
+
+    def build_extensions(self):
+        if self.libuv_force_fetch or self.libuv_clean_compile:
+            self.force = 1
+        self.get_libuv()
+        build_ext.build_extensions(self)
+
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        self.include_dirs.append(os.path.join(self.libuv_dir, 'include'))
+        self.library_dirs.append(self.libuv_dir)
+        if sys.platform.startswith('linux'):
+            self.libraries.append('rt')
+        elif sys.platform == 'darwin':
+            self.extensions[0].extra_link_args = ['-framework', 'CoreServices']
+        self.extensions[0].extra_objects = [os.path.join(self.libuv_dir, 'uv.a')]
+
+    def get_libuv(self):
+        #self.debug_mode =  bool(self.debug) or hasattr(sys, 'gettotalrefcount')
+        def download_libuv():
+            log.info('Downloading libuv...')
+            exec_process(['git', 'clone', '-b', self.libuv_branch, self.libuv_repo, self.libuv_dir])
+            exec_process(['git', 'checkout', self.libuv_revision], cwd=self.libuv_dir)
+        def patch_libuv():
+            log.info('Patching libuv...')
+            for patch_file in self.libuv_patches:
+                exec_process(['patch', '--forward', '-d', self.libuv_dir, '-p0', '-i', os.path.abspath(patch_file)])
+        def build_libuv():
+            cflags = '-fPIC'
+            env = os.environ.copy()
+            env['CFLAGS'] = ' '.join(x for x in (cflags, env.get('CFLAGS', None)) if x)
+            log.info('Building libuv...')
+            exec_process(['make', 'uv.a'], cwd=self.libuv_dir, env=env)
+        if self.libuv_force_fetch:
+            rmtree('deps')
+        if not os.path.exists(self.libuv_dir):
+            download_libuv()
+            patch_libuv()
+            build_libuv()
+        else:
+            if self.libuv_clean_compile:
+                exec_process(['make', 'clean'], cwd=self.libuv_dir)
+            if not os.path.exists(os.path.join(self.libuv_dir, 'uv.a')):
+                log.info('libuv needs to be compiled.')
+                build_libuv()
+            else:
+                log.info('No need to build libuv.')
+
+
+class libuv_sdist(sdist):
+    libuv_dir      = os.path.join('deps', 'libuv')
+    libuv_repo     = libuv_build_ext.libuv_repo
+    libuv_branch   = libuv_build_ext.libuv_branch
+    libuv_revision = libuv_build_ext.libuv_revision
+    libuv_patches  = libuv_build_ext.libuv_patches
+
+    def initialize_options(self):
+        sdist.initialize_options(self)
+        rmtree('deps')
+        makedirs(self.libuv_dir)
+        log.info('Downloading libuv...')
+        exec_process(['git', 'clone', '-b', self.libuv_branch, self.libuv_repo, self.libuv_dir])
+        exec_process(['git', 'checkout', self.libuv_revision], cwd=self.libuv_dir)
+        log.info('Patching libuv...')
+        for patch_file in self.libuv_patches:
+            exec_process(['patch', '--forward', '-d', self.libuv_dir, '-p0', '-i', os.path.abspath(patch_file)])
+        rmtree(os.path.join(self.libuv_dir, '.git'))
+
