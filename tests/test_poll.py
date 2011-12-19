fb97d4e9320acdc2da946eefa9b731d5d033697c
saghul <saghul@gmail.com>
2011-12-19 23:52:05 +0100

    Fixed poll test, use a prepare handle, which will be called *always*

--- tests/test_poll.py
+++ tests/test_poll.py
@@ -8,16 +8,16 @@ import pyuv
 class PollTest(common.UVTestCase):
 
     def test_poll1(self):
-        self.idle_cb_called = 0
-        def idle_cb(idle):
-            self.idle_cb_called += 1
+        self.cb_called = 0
+        def prepare_cb(handle):
+            self.cb_called += 1
         loop = pyuv.Loop.default_loop()
-        idle = pyuv.Idle(loop)
-        idle.start(idle_cb)
+        prepare = pyuv.Prepare(loop)
+        prepare.start(prepare_cb)
         for i in xrange(500):
             loop.poll()
-        idle.close()
-        self.assertEqual(self.idle_cb_called, 500)
+        prepare.close()
+        self.assertEqual(self.cb_called, 500)
 
 
 if __name__ == '__main__':
