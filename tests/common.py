ab5e4e4caa5f17e58f700c2a8cb6bd34d0ef19df
masahitojp <randomstep@gmail.com>
2012-01-18 11:17:06 +0900

    all test succeed on Linux(CentOS)

--- tests/common.py
+++ tests/common.py
@@ -4,7 +4,8 @@ import sys
 sys.path.insert(0, '../')
 
 
-if sys.version_info < (2, 7):
+if sys.version_info < (2, 7) or (0x03000000 <= sys.hexversion < 0x03010000):
+    # py26 or py30
     import unittest2
 else:
     import unittest as unittest2
