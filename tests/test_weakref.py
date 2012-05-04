afd9e091d64ae907fc6e93da88828428132f9a00
saghul <saghul@gmail.com>
2012-05-04 23:36:28 +0200

    Added test for weakrefs

---
+++ tests/test_weakref.py
@@ -0,0 +1,23 @@
+
+import weakref
+
+from common import unittest2
+import pyuv
+
+
+class WeakrefTest(unittest2.TestCase):
+    handle_types = ('Async', 'Check', 'Idle', 'Pipe', 'Prepare', 'TCP', 'Timer', 'UDP')
+
+    def test_weakref(self):
+        loop = pyuv.Loop.default_loop()
+        for type in self.handle_types:
+            klass = getattr(pyuv, type)
+            obj = klass(loop)
+            weak = weakref.ref(obj)
+            del obj
+            self.assertEqual(weak(), None)
+
+
+if __name__ == '__main__':
+    unittest2.main(verbosity=2)
+
