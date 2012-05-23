2253de1a21bdd9dfbad03a8487832c00d4bae082
saghul <saghul@gmail.com>
2012-05-23 22:59:58 +0200

    Raised libuv revision

--- setup_libuv.py
+++ setup_libuv.py
@@ -50,7 +50,7 @@ class libuv_build_ext(build_ext):
     libuv_dir      = os.path.join('deps', 'libuv')
     libuv_repo     = 'https://github.com/joyent/libuv.git'
     libuv_branch   = 'master'
-    libuv_revision = '0ef7844'
+    libuv_revision = 'e71495c'
     libuv_patches  = ['patches/c-ares_naptr_support.patch']
 
     user_options = build_ext.user_options
