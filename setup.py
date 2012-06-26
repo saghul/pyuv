234cf427ee39a178cc5992e022b60fc50c16cbfd
saghul <saghul@gmail.com>
2012-06-26 22:08:40 +0200

    Raised version and updated changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 from setup_libuv import libuv_build_ext, libuv_sdist
 
 
-__version__ = "0.7.2"
+__version__ = "0.8.0"
 
 setup(name             = "pyuv",
       version          = __version__,
