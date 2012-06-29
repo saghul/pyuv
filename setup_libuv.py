0f3e0e622209dc5b7aca7b686856ef10c59131fb
saghul <saghul@gmail.com>
2012-06-29 14:49:58 +0200

    Raised libuv revision
    Also removed c-ares patch for NAPTR support, since libuv
    now bundles c-ares 1.9.0 and it alreasy includes it.

--- setup_libuv.py
+++ setup_libuv.py
@@ -50,8 +50,8 @@ class libuv_build_ext(build_ext):
     libuv_dir      = os.path.join('deps', 'libuv')
     libuv_repo     = 'https://github.com/joyent/libuv.git'
     libuv_branch   = 'master'
-    libuv_revision = 'c6f2ef2'
-    libuv_patches  = ['patches/c-ares_naptr_support.patch']
+    libuv_revision = '700f133'
+    libuv_patches  = []
 
     user_options = build_ext.user_options
     user_options.extend([
