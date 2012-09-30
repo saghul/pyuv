e346d61663a3ac9665f8d6e6ceacb42a773c00cf
saghul <saghul@gmail.com>
2012-09-30 20:24:30 +0200

    Raised libuv revision

--- setup_libuv.py
+++ setup_libuv.py
@@ -50,7 +50,7 @@ class libuv_build_ext(build_ext):
     libuv_dir      = os.path.join('deps', 'libuv')
     libuv_repo     = 'https://github.com/joyent/libuv.git'
     libuv_branch   = 'master'
-    libuv_revision = 'cc1c191'
+    libuv_revision = '7ac23ee'
     libuv_patches  = []
 
     user_options = build_ext.user_options
