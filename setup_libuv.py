1a7d20d4926eacedcd2f81b87ec60b659aafb1a9
saghul <saghul@gmail.com>
2012-10-24 15:18:42 +0200

    Raised libuv revision
    Should fix the build

--- setup_libuv.py
+++ setup_libuv.py
@@ -50,7 +50,7 @@ class libuv_build_ext(build_ext):
     libuv_dir      = os.path.join('deps', 'libuv')
     libuv_repo     = 'https://github.com/joyent/libuv.git'
     libuv_branch   = 'master'
-    libuv_revision = '61ecb34'
+    libuv_revision = 'b7f38b1'
     libuv_patches  = []
 
     user_options = build_ext.user_options
