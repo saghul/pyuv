e68e570f09abd31472956759bc51c77631cf274b
saghul <saghul@gmail.com>
2012-11-16 22:57:01 +0100

    Now working on 0.9.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 from setup_libuv import libuv_build_ext, libuv_sdist
 
 
-__version__ = "0.9.2"
+__version__ = "0.9.3.dev"
 
 setup(name             = "pyuv",
       version          = __version__,
