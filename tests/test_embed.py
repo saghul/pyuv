19215f6cbe00ae2f970555d668ea371380f010d5
saghul <saghul@gmail.com>
2012-11-29 22:48:43 +0100

    Added test for loop embedding

---
+++ tests/test_embed.py
@@ -0,0 +1,78 @@
+
+from common import unittest2
+from threading import Thread
+
+import pyuv
+
+import errno
+import select
+
+try:
+    poller = select.epoll
+    poller_type = 'epoll'
+except AttributeError:
+    try:
+        poller = select.kqueue
+        poller_type = 'kqueue'
+    except AttributeError:
+        poller = None
+
+
+class EmbedTest(unittest2.TestCase):
+
+    def embed_cb(self, handle):
+        self.loop.run_once()
+        self.sem.post()
+
+    def timer_cb(self, handle):
+        self.embed_timer_called += 1
+        self.embed_closed = True
+        self.embed_async.close()
+        handle.close()
+
+    def embed_runner(self):
+        fd = self.loop.fileno()
+        poll = poller.fromfd(fd)
+        while not self.embed_closed:
+            timeout = self.loop.get_timeout()
+            while True:
+                try:
+                    if poller_type == 'kqueue':
+                        poll.control(None, 0, timeout)
+                    elif poller_type == 'epoll':
+                        poll.poll(timeout)
+                    else:
+                        self.fail('Bogus poller type')
+                except OSError as e:
+                    if e.args[0] == errno.EINTR:
+                        continue
+                break
+            self.embed_async.send()
+            self.sem.wait()
+
+    def test_embed(self):
+        if poller is None:
+            self.skipTest("test disabled if no suitable poller method is found")
+            return
+        self.embed_timer_called = 0
+        self.embed_closed = False
+        self.external = pyuv.Loop()
+        self.embed_async = pyuv.Async(self.external, self.embed_cb)
+
+        self.loop = pyuv.Loop()
+        timer = pyuv.Timer(self.loop)
+        timer.start(self.timer_cb, 0.25, 0)
+
+        self.sem = pyuv.thread.Semaphore(0)
+        t = Thread(target=self.embed_runner)
+        t.start()
+        self.external.run()
+        t.join()
+        external = None
+
+        self.assertEquals(self.embed_timer_called, 1)
+
+
+if __name__ == '__main__':
+    unittest2.main(verbosity=2)
+
