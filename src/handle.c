8e8315fbbbb853ea6aa572d611b0a8c25b9c3600
Geert Jansen <geertj@gmail.com>
2013-01-11 17:25:27 +0100

    Handle doesn't need to be inheritable.

--- src/handle.c
+++ src/handle.c
@@ -246,7 +246,7 @@ static PyTypeObject HandleType = {
     0,                                                             /*tp_getattro*/
     0,                                                             /*tp_setattro*/
     0,                                                             /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, /*tp_flags*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,                       /*tp_flags*/
     0,                                                             /*tp_doc*/
     (traverseproc)Handle_tp_traverse,                              /*tp_traverse*/
     (inquiry)Handle_tp_clear,                                      /*tp_clear*/
