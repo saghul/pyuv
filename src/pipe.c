fdd44ad7b20d4f11e38831f9ca280dc1f7b220e0
Geert Jansen <geertj@gmail.com>
2013-01-11 15:28:53 +0100

    Allow Python classes to inherit from pyuv types.

--- src/pipe.c
+++ src/pipe.c
@@ -463,7 +463,7 @@ static PyTypeObject PipeType = {
     0,                                                             /*tp_getattro*/
     0,                                                             /*tp_setattro*/
     0,                                                             /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,                       /*tp_flags*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, /*tp_flags*/
     0,                                                             /*tp_doc*/
     (traverseproc)Pipe_tp_traverse,                                /*tp_traverse*/
     (inquiry)Pipe_tp_clear,                                        /*tp_clear*/
