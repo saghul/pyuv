b4511defcd1fef52b8e8b1b06cb6f289106fe1eb
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-01-24 23:31:38 +0100

    Now working on version 0.9.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 from setup_libuv import libuv_build_ext, libuv_sdist
 
 
-__version__ = "0.9.5"
+__version__ = "0.9.6.dev"
 
 setup(name             = "pyuv",
       version          = __version__,
