69fbc692c46f7f8b54e73330e7ab92ea48759759
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-09-05 22:17:16 +0200

    Use ARCHFLAGS if set when compiling libuv
    Fixes #131

--- setup_libuv.py
+++ setup_libuv.py
@@ -159,7 +159,7 @@ class libuv_build_ext(build_ext):
         def build_libuv():
             cflags = '-fPIC'
             env = os.environ.copy()
-            env['CFLAGS'] = ' '.join(x for x in (cflags, env.get('CFLAGS', None)) if x)
+            env['CFLAGS'] = ' '.join(x for x in (cflags, env.get('CFLAGS', None), env.get('ARCHFLAGS', None)) if x)
             log.info('Building libuv...')
             if win32_msvc:
                 prepare_windows_env(env)
