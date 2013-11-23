ef5d7cbe324e1cf5ca0f571cfe306fc0edf564d4
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-11-23 20:02:47 +0100

    Raised version and updated changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 from setup_libuv import libuv_build_ext, libuv_sdist
 
 
-__version__ = "0.10.7"
+__version__ = "0.10.8"
 
 setup(name             = "pyuv",
       version          = __version__,
