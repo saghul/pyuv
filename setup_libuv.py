0a945dc741613f966a67fcf4a3558f0cb1c31e29
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-09-13 10:08:31 +0200

    core: raised libuv revision and switched to v1.x branch

--- setup_libuv.py
+++ setup_libuv.py
@@ -93,8 +93,8 @@ def prepare_windows_env(env):
 class libuv_build_ext(build_ext):
     libuv_dir      = os.path.join('deps', 'libuv')
     libuv_repo     = 'https://github.com/joyent/libuv.git'
-    libuv_branch   = 'master'
-    libuv_revision = '06f9e14'
+    libuv_branch   = 'v1.x'
+    libuv_revision = 'f70b3fa'
     libuv_patches  = []
 
     user_options = build_ext.user_options
