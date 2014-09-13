3b505af5be211e1ca154b13dc9c44faa62e46e00
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-09-13 10:09:20 +0200

    version: set version to 1.0.0-dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 from setup_libuv import libuv_build_ext, libuv_sdist
 
 
-__version__ = "0.11.5"
+__version__ = "1.0.0-dev"
 
 setup(name             = "pyuv",
       version          = __version__,
