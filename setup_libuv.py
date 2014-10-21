fce601f5796d52fb352b2bf57c1a2553befafafc
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-10-21 21:42:53 +0200

    core: raise libuv revision
    To libuv version 1.0.0-rc2

--- setup_libuv.py
+++ setup_libuv.py
@@ -94,7 +94,7 @@ class libuv_build_ext(build_ext):
     libuv_dir      = os.path.join('deps', 'libuv')
     libuv_repo     = 'https://github.com/joyent/libuv.git'
     libuv_branch   = 'v1.x'
-    libuv_revision = 'f70b3fa'
+    libuv_revision = 'e3ea6c1'
     libuv_patches  = []
 
     user_options = build_ext.user_options
