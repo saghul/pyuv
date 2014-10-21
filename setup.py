93894ccdb5a8a12b75e60e288b50d1aaf3dbeb5b
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-10-21 21:35:23 +0200

    core: raised version and updated changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 from setup_libuv import libuv_build_ext, libuv_sdist
 
 
-__version__ = "0.10.11"
+__version__ = "0.10.12"
 
 setup(name             = "pyuv",
       version          = __version__,
