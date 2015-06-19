1d8aab4c7dcdd56404c9853afb3bd2db65069745
Saúl Ibarra Corretgé <saghul@gmail.com>
2015-06-19 11:59:11 +0200

    build: fix linking problem on Windows
    The current libuv needs to link with shell32 and userenv libraries.
    
    Ref: https://github.com/saghul/pyuv/issues/200

--- setup_libuv.py
+++ setup_libuv.py
@@ -155,6 +155,8 @@ class libuv_build_ext(build_ext):
             self.compiler.add_library('advapi32')
             self.compiler.add_library('iphlpapi')
             self.compiler.add_library('psapi')
+            self.compiler.add_library('shell32')
+            self.compiler.add_library('userenv')
             self.compiler.add_library('ws2_32')
         elif sys.platform.startswith('freebsd'):
             self.compiler.add_library('kvm')
