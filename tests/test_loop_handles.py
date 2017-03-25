8e1c5a6c0510d0957a19f7b0cd49e1cdf4ba4cc7
Randall Leeds <randall@bleeds.info>
2017-03-24 17:00:26 -0700

    test: convert weakref into handle lifetime test
    The gc test already tests that weakrefs work and are cleaned up when
    they participate in cycles. Instead of testing that weakrefs survive
    until a handle is deleted, which depends on implementation, check that
    the handles exist on the loop and they are marked as closed.

--- tests/test_loop_handles.py
+++ tests/test_loop_handles.py
@@ -8,6 +8,7 @@ import pyuv
 
 
 class HandlesTest(TestCase):
+    handle_types = ('Check', 'Idle', 'Pipe', 'Prepare', 'TCP', 'Timer', 'UDP')
 
     def test_handles(self):
         timer = pyuv.Timer(self.loop)
@@ -16,6 +17,33 @@ class HandlesTest(TestCase):
         timer = None
         self.loop.run()
         self.assertFalse(self.loop.handles)
+
+    def test_handle_lifetime(self):
+        refs = []
+        for type in self.handle_types:
+            klass = getattr(pyuv, type)
+            obj = klass(self.loop)
+            refs.append(weakref.ref(obj))
+            del obj
+
+        # There are no more references to the handles at this point.
+        # Garbage collection should be prevented from freeing them, though.
+        # Touching each of these without segfault is a best effort check.
+        # The validity of the weakrefs is implementation dependent :(.
+        gc.collect()
+        handles = self.loop.handles
+        self.assertEqual(len(handles), len(self.handle_types))
+        for handle in handles:
+            self.assertTrue(handle.closed)
+            del handle
+        del handles
+
+        # Give the loop a chance to finish closing the handles.
+        self.loop.run()
+
+        # Ensure the weakref is gone now.
+        for ref in refs:
+            self.assertEqual(ref(), None)
 
 
 if __name__ == '__main__':
