9d05d462735b9b76cc033cd8c3dc23f1cabe9db4
Randall Leeds <randall@bleeds.info>
2017-03-25 13:56:05 -0700

    ci: add pypy to tox and travis

---
+++ .travis/before_install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+cd $HOME
+
+if [[ $TRAVIS_PYTHON_VERSION == 'pypy' ]]; then
+    deactivate
+
+    wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-5.7-linux_x86_64-portable.tar.bz2
+    tar -jxvf pypy-5.7-linux_x86_64-portable.tar.bz2
+    cd pypy-5.7-linux_x86_64-portable
+
+    echo 'Setting up aliases...'
+    export PATH=$HOME/pypy-5.7-linux_x86_64-portable/bin/:$PATH
+    ln -s ~/pypy-5.7-linux_x86_64-portable/bin/pypy bin/python
+
+    echo 'Setting up pip...'
+    bin/pypy -m ensurepip
+
+    cd $TRAVIS_BUILD_DIR
+fi
+
+if [[ $TRAVIS_PYTHON_VERSION == 'pypy3' ]]; then
+    deactivate
+
+    wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-5.7-beta-linux_x86_64-portable.tar.bz2
+    tar -jxvf pypy3.5-5.7-beta-linux_x86_64-portable.tar.bz2
+    cd pypy3.5-5.7-beta-linux_x86_64-portable
+
+    echo 'Setting up aliases...'
+    export PATH=$HOME/pypy3.5-5.7-beta-linux_x86_64-portable/bin/:$PATH
+    ln -s ~/pypy3.5-5.7-beta-linux_x86_64-portable/bin/pypy3.5 bin/python
+
+    echo 'Setting up pip...'
+    bin/pypy -m ensurepip
+    ln -s ~/pypy3.5-5.7-beta-linux_x86_64-portable/bin/pip3.5 bin/pip
+fi
+
+cd $TRAVIS_BUILD_DIR
