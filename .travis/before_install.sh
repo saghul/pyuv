4738c5d41126b4003e47f31f12185d3247358166
Marc Schlaich <marc.schlaich@googlemail.com>
2017-06-09 09:36:39 +0200

    ci: use new PyPy version on Travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,34 +6,32 @@ cd $HOME
 if [[ $TRAVIS_PYTHON_VERSION == 'pypy' ]]; then
     deactivate
 
-    wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-5.7-linux_x86_64-portable.tar.bz2
-    tar -jxvf pypy-5.7-linux_x86_64-portable.tar.bz2
-    cd pypy-5.7-linux_x86_64-portable
+    wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-5.8-linux_x86_64-portable.tar.bz2
+    tar -jxvf pypy-5.8-linux_x86_64-portable.tar.bz2
+    cd pypy-5.8-linux_x86_64-portable
 
     echo 'Setting up aliases...'
-    export PATH=$HOME/pypy-5.7-linux_x86_64-portable/bin/:$PATH
-    ln -s ~/pypy-5.7-linux_x86_64-portable/bin/pypy bin/python
+    export PATH=$HOME/pypy-5.8-linux_x86_64-portable/bin/:$PATH
+    ln -s ~/pypy-5.8-linux_x86_64-portable/bin/pypy bin/python
 
     echo 'Setting up pip...'
     bin/pypy -m ensurepip
-
-    cd $TRAVIS_BUILD_DIR
 fi
 
 if [[ $TRAVIS_PYTHON_VERSION == 'pypy3' ]]; then
     deactivate
 
-    wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-5.7-beta-linux_x86_64-portable.tar.bz2
-    tar -jxvf pypy3.5-5.7-beta-linux_x86_64-portable.tar.bz2
-    cd pypy3.5-5.7-beta-linux_x86_64-portable
+    wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-5.8-beta-linux_x86_64-portable.tar.bz2
+    tar -jxvf pypy3.5-5.8-beta-linux_x86_64-portable.tar.bz2
+    cd pypy3.5-5.8-beta-linux_x86_64-portable
 
     echo 'Setting up aliases...'
-    export PATH=$HOME/pypy3.5-5.7-beta-linux_x86_64-portable/bin/:$PATH
-    ln -s ~/pypy3.5-5.7-beta-linux_x86_64-portable/bin/pypy3.5 bin/python
+    export PATH=$HOME/pypy3.5-5.8-beta-linux_x86_64-portable/bin/:$PATH
+    ln -s ~/pypy3.5-5.8-beta-linux_x86_64-portable/bin/pypy3.5 bin/python
 
     echo 'Setting up pip...'
     bin/pypy -m ensurepip
-    ln -s ~/pypy3.5-5.7-beta-linux_x86_64-portable/bin/pip3.5 bin/pip
+    ln -s ~/pypy3.5-5.8-beta-linux_x86_64-portable/bin/pip3.5 bin/pip
 fi
 
 cd $TRAVIS_BUILD_DIR
