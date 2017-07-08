639e2d6676899981105f99be00639b51a83cc168
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-07-08 13:40:56 +0200

    core: set version to 1.4.0

--- build-manylinux-wheels.sh
+++ build-manylinux-wheels.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 # This script runs inside Docker
 
-set -e
+set -e -x
 
-
-# Dependencies
-yum install -y libtool
-ARCH=$(uname -m)
-if [[ $ARCH == "x86_64" ]]; then
-    wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/tpokorra:/openpetra/CentOS_CentOS-5/x86_64/autoconf-2.69-3.2.x86_64.rpm
-    yum install -y --nogpgcheck autoconf*x86_64.rpm
-else
-    wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/tpokorra:/openpetra/CentOS_CentOS-5/i386/autoconf-2.69-3.2.i386.rpm
-    yum install -y --nogpgcheck autoconf*i386.rpm
-fi
-/opt/python/cp35-cp35m/bin/pip install -U auditwheel
 
 # Cleanup
 cd /pyuv
 rm -rf wheeltmp
 
 # Build
-for version in cp27-cp27m cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m; do
-    /opt/python/${version}/bin/python setup.py build_ext --libuv-clean-compile
-    /opt/python/${version}/bin/pip wheel . -w wheeltmp
+for PYBIN in /opt/python/*/bin; do
+    "$PYBIN/python" setup.py build_ext
+    "$PYBIN/pip" wheel . -w wheeltmp
 done
 for whl in wheeltmp/*.whl; do
     auditwheel repair $whl -w wheelhouse
@@ -34,10 +22,10 @@ rm -rf wheeltmp
 cd ..
 
 # Test (ignore if some fail, we just want to know that the module works)
-for version in cp27-cp27m cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m; do
-    /opt/python/${version}/bin/python --version
-    /opt/python/${version}/bin/pip install pyuv --no-index -f /pyuv/wheelhouse
-    /opt/python/${version}/bin/python -c "import pyuv; print('%s - %s' % (pyuv.__version__, pyuv.LIBUV_VERSION));"
+for PYBIN in /opt/python/*/bin; do
+    "$PYBIN/python" --version
+    "$PYBIN/pip" install pyuv --no-index -f /pyuv/wheelhouse
+    "$PYBIN/python" -c "import pyuv; print('%s - %s' % (pyuv.__version__, pyuv.LIBUV_VERSION));"
 done
 
 exit 0
